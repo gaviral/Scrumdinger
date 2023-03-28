14ebee9081d9fa77d0182fead1a05ad013070582
Aviral Garg <aviral.garg@icloud.com>
2023-03-28 14:32:15 -0700

    Iterate through attendees
    Add attendees list to the details screen.
    
    Steps:
    - Extend DailyScrum with an inner structure named Attendee that is identifiable (follows the `Identifiable` protocol).
    - Iterate through each attendee and add a label for them in a new section in DetailView.

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -33,6 +33,11 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            Section(header: Text("Attendee")){
+                ForEach(scrum.attendees) {attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
     }
 }
